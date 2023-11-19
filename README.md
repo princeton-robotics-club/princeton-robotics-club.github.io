6f0faf8407f6e2a12ea370daef829b5fb1a634e2
lglisic <lana.glisic@gmail.com>
2023-11-18 21:53:34 -0500

    rover build error fix

--- README.md
+++
@@ -1,1 +0,0 @@
-Readme (Work in Progress)
--- postProcess.mjs
+++
@@ -1,6 +0,0 @@
-import fs from 'fs';
-fs.copyFileSync('build/index.html', 'build/projects.html');
-fs.copyFileSync('build/index.html', 'build/about.html');
-fs.copyFileSync('build/index.html', 'build/supporters.html');
-fs.copyFileSync('build/index.html', 'build/join.html');
-fs.copyFileSync('build/index.html', 'build/404.html');
